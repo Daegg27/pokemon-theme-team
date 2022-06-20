b523a79263d6a12e6a265e1f51bb4c313d7442e2
Dalton Eggleston <egglestondalton@yahoo.com>
2022-06-20 15:22:57 -0700

    done

--- app.js
+++ app.js
@@ -9,40 +9,77 @@
 
 const getPokeBois = async (event) => {
     event.preventDefault()
+
     const boxes = document.querySelectorAll(".show-types")
     boxes.forEach(box => {
         box.remove();
     });
 
-    try {
+    
         let response = await axios.get('https://pokeapi.co/api/v2/pokemon/')
         let pokeNum = response.data.count
         let randomNum = Math.floor(Math.random() * pokeNum)
         response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
         console.log(response)
-        let pokeType = response.data.types[0].type['name']
-        let pokeImage = response.data.sprites.front_default
+        let pokeType = response.data.types[0].type['name'] // This could technically be a constant, unless you wanted to keep tracking
+        let pokeImage = response.data.sprites.front_default // This what will keep track of the current image needed
         console.log(pokeType)
         document.getElementById('first-picture').setAttribute("src", `${pokeImage}`)
         var d = document.createElement("div")
         d.setAttribute("class", "show-types" )
         document.getElementById('column-one').appendChild(d)
         d.innerText = `${response.data.name}: ${pokeType}`
-        // let pokeList = []
+        let pokeList = []
         response = await axios.get(`https://pokeapi.co/api/v2/type/${pokeType}`)
-        // pokeNum = response.data.pokemon.length;
-        // randomNum = Math.floor(Math.random() * pokeNum)
-        // for (i = 0; i <= 5; i++){
-
-        // }
-        // console.log(pokeNum)
-        console.log(response)
+        // console.log(response.data.pokemon[0].pokemon)
+        pokeNum = response.data.pokemon.length;
+        for (i = 0; i < 5; i++){
+            randomNum = Math.floor(Math.random() * pokeNum)
+            pokeList.push(response.data.pokemon[randomNum].pokemon) // Creates a list of the pokemon's name % URL in API
+        }
+        response = await axios.get(pokeList[0].url) // Creates the request for the second Pokemon
+        // console.log(response)
+        pokeImage = response.data.sprites.front_default
+        document.getElementById("second-picture").setAttribute("src", `${pokeImage = response.data.sprites.front_default}`)
+        d = document.createElement("div")
+        d.setAttribute("class", "show-types" )
+        document.getElementById('column-two').appendChild(d)
+        d.innerText = `${response.data.name}: ${pokeType}`
+        response = await axios.get(pokeList[1].url) // Creates the request for the third Pokemon
+        pokeImage = response.data.sprites.front_default
+        document.getElementById("third-picture").setAttribute("src", `${pokeImage = response.data.sprites.front_default}`)
+        d = document.createElement("div")
+        d.setAttribute("class", "show-types" )
+        document.getElementById('column-three').appendChild(d)
+        d.innerText = `${response.data.name}: ${pokeType}`
+        response = await axios.get(pokeList[2].url) // Creates the request for the fourth Pokemon
+        pokeImage = response.data.sprites.front_default
+        document.getElementById("fourth-picture").setAttribute("src", `${pokeImage = response.data.sprites.front_default}`)
+        d = document.createElement("div")
+        d.setAttribute("class", "show-types" )
+        document.getElementById('column-four').appendChild(d)
+        d.innerText = `${response.data.name}: ${pokeType}`
+        response = await axios.get(pokeList[3].url) // Creates the request for the fifth Pokemon
+        pokeImage = response.data.sprites.front_default
+        document.getElementById("fifth-picture").setAttribute("src", `${pokeImage = response.data.sprites.front_default}`)
+        d = document.createElement("div")
+        d.setAttribute("class", "show-types" )
+        document.getElementById('column-five').appendChild(d)
+        d.innerText = `${response.data.name}: ${pokeType}`
+        response = await axios.get(pokeList[4].url) // Creates the request for the fourth Pokemon
+        pokeImage = response.data.sprites.front_default
+        document.getElementById("sixth-picture").setAttribute("src", `${pokeImage = response.data.sprites.front_default}`)
+        d = document.createElement("div")
+        d.setAttribute("class", "show-types" )
+        document.getElementById('column-six').appendChild(d)
+        d.innerText = `${response.data.name}: ${pokeType}`
+        
+        // console.log("As requested:", pokeList)
+        // console.log(response)
         
     }
-    catch(e){
-        console.log('NOPE, NICE TRY')
-    }
-}
+    
+
 
 function GFG_Fun(event) {
     event.preventDefault()
