7298c9b62810ea5aba3da261ede5129446a79c77
Dalton Eggleston <egglestondalton@yahoo.com>
2022-06-20 13:18:26 -0700

    need the five

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
-// const getPokeBois = async () => {
-//     try {
-//         let response = await axios.get('https://pokeapi.co/api/v2/pokemon/')
-//         const pokeNum = response.data.count
-//         const randomNum = Math.floor(Math.random() * pokeNum)
-//         response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
-//         console.log(response)
-//         let pokeType = response.data.types[0].type['name']
-//         console.log(pokeType)
-//         let pokeList = []
-//         response = await axios.get(`https://pokeapi.co/api/v2/type/${pokeType}`)
-//         console.log(response)
-        
-//     }
-//     catch(e){
-//         console.log('NOPE, NICE TRY')
-//         console.log(e)
-//     }
+// function getNameAndImage (object) {
+//     console.log(object)
+//     let name=object.data.name
+//     let imageURL= object.data.sprites.front_default
+//     return [name,imageURL]
 // }
+
+
+
+const getPokeBois = async (event) => {
+    event.preventDefault()
+    const boxes = document.querySelectorAll(".show-types")
+    boxes.forEach(box => {
+        box.remove();
+    });
+
+    try {
+        let response = await axios.get('https://pokeapi.co/api/v2/pokemon/')
+        let pokeNum = response.data.count
+        let randomNum = Math.floor(Math.random() * pokeNum)
+        response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
+        console.log(response)
+        let pokeType = response.data.types[0].type['name']
+        let pokeImage = response.data.sprites.front_default
+        console.log(pokeType)
+        document.getElementById('first-picture').setAttribute("src", `${pokeImage}`)
+        var d = document.createElement("div")
+        d.setAttribute("class", "show-types" )
+        document.getElementById('column-one').appendChild(d)
+        d.innerText = `${response.data.name}: ${pokeType}`
+        // let pokeList = []
+        response = await axios.get(`https://pokeapi.co/api/v2/type/${pokeType}`)
+        // pokeNum = response.data.pokemon.length;
+        // randomNum = Math.floor(Math.random() * pokeNum)
+        // for (i = 0; i <= 5; i++){
+
+        // }
+        // console.log(pokeNum)
+        console.log(response)
+        
+    }
+    catch(e){
+        console.log('NOPE, NICE TRY')
+    }
+}
 
 function GFG_Fun(event) {
     event.preventDefault()
